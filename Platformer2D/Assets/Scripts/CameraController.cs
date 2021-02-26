1216cfa0a7fedff93401a7f9ec0bdfa8dca2ce44
U-ISART\m.mehalin <m.mehalin@Zotac-GP-24.isart.lan>
2021-02-26 16:49:37 +0100

    character in progress

---
+++ Platformer2D/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player = null;
+    [SerializeField]
+    private Camera myCamera = null;
+
+    private float camera_speed = 5.0f;
+    private float camera_posY;
+    private float camera_posZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera_posY = myCamera.transform.position.y;
+        camera_posZ = myCamera.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 destination = new Vector3(player.transform.position.x, camera_posY, camera_posZ);
+        myCamera.transform.position = Vector3.Lerp(myCamera.transform.position, destination, camera_speed);
+    }
+}
--- Platformer2D/Assets/Scripts/Player.cs
+++ Platformer2D/Assets/Scripts/Player.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody rb;
-    private float moveSpeed = 10.0f;
-    private bool isJumping = false;
-    private int Ground = 10;
     [SerializeField]
-    private uint life = 5;
+    private float moveSpeed = 8.0f;
+    [SerializeField]
+    private float jumpForce = 6.0f;
+    private bool isJumping;
+    [SerializeField]
+    private int life = 5;
 
     private void Awake()
     {
@@ -19,7 +21,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -31,37 +33,45 @@ public class Player : MonoBehaviour
     private void Move()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        float jumpInput = Input.GetAxis("Jump");
 
-        if (jumpInput > 0 && isJumping == false)
-            rb.AddForce(new Vector3(0, 1, 0) * 1.0f, ForceMode.Impulse);
+        transform.LookAt(transform.position + new Vector3(0, 0, horizontalInput));
 
-        transform.Translate(new Vector3(horizontalInput, 0.0f, 0.0f) * moveSpeed * Time.deltaTime);
+        rb.velocity = new Vector3(horizontalInput * moveSpeed, rb.velocity.y, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+            rb.AddForce(new Vector3(0.0f, 1.0f, 0.0f) * jumpForce, ForceMode.Impulse);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
+            Vector3 velocity = collision.relativeVelocity.normalized;
+        }
+
+        if (collision.gameObject.tag == "Enemy")
+        {
             --life;
 
-            if (life <= 0)
-                Destroy(gameObject);
+            transform.position = new Vector3(GameObject.FindWithTag("Checkpoint").transform.position.x, 0, 0);
+
+            //if (life <= 0)
         }
 
         if (collision.gameObject.tag == "MovingPlatform")
             transform.SetParent(collision.transform);
+    }
 
-        if (collision.gameObject.layer == Ground)
-            isJumping = false;
-    }   
+    private void OnCollisionStay(Collision collision)
+    {
+        isJumping = false;
+    }
 
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.tag == "MovingPlatform")
             transform.SetParent(null);
 
-        if (collision.gameObject.layer == Ground)
-            isJumping = true;
+        isJumping = true;
     }
 }
