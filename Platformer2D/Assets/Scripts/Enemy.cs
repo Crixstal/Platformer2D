c36cd1fd7960cf6426903a4fbd3373941cb1fad4
U-ISART\m.mehalin <m.mehalin@Zotac-GP-24.isart.lan>
2021-03-10 17:35:56 +0100

    enemy follow player in progress

--- Platformer2D/Assets/Scripts/Enemy.cs
+++ Platformer2D/Assets/Scripts/Enemy.cs
@@ -1,24 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform player;
+    private Rigidbody rb;
     [SerializeField]
-    private float moveSpeed = 8.0f;
+    private float force = 5.0f;
+
     private void Awake()
     {
-
-    }
-
-    void Update()
-    {
-        //Move();
-    }
-
-    private void Move()
-    {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -28,5 +22,17 @@ public class Enemy : MonoBehaviour
             if (collision.GetContact(0).normal == new Vector3(0f, -1f, 0f))
                 Destroy(gameObject);
         }
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            transform.LookAt(player);
+
+            //transform.position = Vector3.MoveTowards(transform.position, player.position, force * Time.deltaTime);
+            rb.AddForce(-player.position * force, ForceMode.Impulse);
+        }
+
     }
 }
